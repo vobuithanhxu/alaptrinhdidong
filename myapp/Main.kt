26a3d40276fce63e12feb1cd25fcf5b44efc7847
vobuithanhxu <144611014+vobuithanhxu@users.noreply.github.com>
2024-03-20 13:23:25 +0700

    Add files via upload

---
+++ myapp/Main.kt
@@ -0,0 +1,57 @@
+package example.myapp
+
+import example.myapp.decor.Decoration
+
+fun buildAquarium() {
+    val aquarium1 = Aquarium()
+    aquarium1.printSize()
+    // default height and length
+    val aquarium2 = Aquarium(width = 25)
+    aquarium2.printSize()
+    // default width
+    val aquarium3 = Aquarium(height = 35, length = 110)
+    aquarium3.printSize()
+    // everything custom
+    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+    aquarium4.printSize()
+
+    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
+    aquarium6.printSize()
+
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+    myAquarium.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
+
+
+
+
+}
+
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
+    println("Shark: ${shark.color}")
+    shark.eat()
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
+}
+
+fun makeDecorations() {
+    val decoration1 = Decoration("granite")
+    println(decoration1)
+
+    val decoration2 = Decoration("slate")
+    println(decoration2)
+
+    val decoration3 = Decoration("slate")
+    println(decoration3)
+    println (decoration1.equals(decoration2))
+    println (decoration3.equals(decoration2))
+}
+
+fun main() {
+//    buildAquarium()
+ //   makeFish()
+    makeDecorations()
+}
